ec111d453b856a6368e4962d81f4a9133e3c84ff
Aminul <aminul.sabeer@proton.me>
2026-02-17 15:29:50 +0530

    some changes

--- duplicate_num_check.c
+++ duplicate_num_check.c
@@ -1,22 +1,32 @@
-#include<stdio.h>
-int main(){
-    int n,count=0;
-    printf("Enter no. of digits: ");
-    scanf("%d",&n);
-    int arr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
-    }
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(arr[i]==arr[j]){
-                count++;
-                break;
-            }
+#include <stdio.h>
+
+int main() {
+    long int n;
+    int digit;
+    int freq[10] = {0};   // stores count of digits
+    int duplicate = 0;
+
+    printf("Enter a number: ");
+    scanf("%ld", &n);
+
+    if(n < 0) n = -n;   // handle negative numbers
+
+    while(n > 0) {
+        digit = n % 10;
+
+        if(freq[digit] == 1) {
+            duplicate = 1;
+            break;
         }
+
+        freq[digit] = 1;
+        n = n / 10;
     }
-    if(count>0){
-        printf("Yes");
-    }
+
+    if(duplicate)
+        printf("Duplicate digits found\n");
+    else
+        printf("No duplicate digits\n");
+
     return 0;
 }
